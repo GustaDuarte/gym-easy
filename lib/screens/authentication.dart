86a698e8dc2a87d50f1103d3a90682706102cda4
gustavo duarte <gustavo.7@rede.ulbra.br>
2025-09-08 16:49:33 -0300

    :sparkles: feat: authentication form created

--- lib/screens/authentication.dart
+++ lib/screens/authentication.dart
@@ -1,8 +1,15 @@
 import 'package:flutter/material.dart';
 import 'package:flutter_projects/_core/my_colors.dart';
 
-class AuthenticationScreen extends StatelessWidget {
+class AuthenticationScreen extends StatefulWidget {
   const AuthenticationScreen({super.key});
+
+  @override
+  State<AuthenticationScreen> createState() => _AuthenticationScreenState();
+}
+
+class _AuthenticationScreenState extends State<AuthenticationScreen> {
+  bool getIn = true;
 
   @override
   Widget build(BuildContext context) {
@@ -17,12 +24,50 @@ class AuthenticationScreen extends StatelessWidget {
             MyColors.backgroundApp,
             MyColors.backgroundCards,
           ])),),
-          Column(
-            mainAxisAlignment: MainAxisAlignment.center,
-            crossAxisAlignment: CrossAxisAlignment.stretch,
-            children: [
-              Image.asset("assets/logo.png", height: 200, ),
-            ],
+          Padding(
+            padding: const EdgeInsets.all(16.0),
+            child: Form(
+              child: SingleChildScrollView(
+                child: Column(
+                  mainAxisAlignment: MainAxisAlignment.center,
+                  crossAxisAlignment: CrossAxisAlignment.stretch,
+                  children: [
+                    Image.asset("assets/logo.png", height: 200),
+                    SizedBox(height: 32,),
+                    TextFormField(decoration: InputDecoration(label: Text("Email"),
+                    ),
+                    ),
+                    TextFormField(decoration: InputDecoration(label: Text("Senha"),
+                    ),
+                      obscureText: true,
+                    ),
+                    Visibility(visible: !getIn, child: Column(
+                      children: [
+                        TextFormField(decoration: InputDecoration(label: Text("Confirme Senha"),
+                        ),
+                          obscureText: true,
+                        ),
+                        TextFormField(decoration: InputDecoration(label: Text("Nome"),
+                        ),),
+                      ],
+                    )),
+                    SizedBox(height: 32,),
+                    ElevatedButton(
+                      onPressed: () {},
+                      child: Text((getIn)? "Entrar" : "Cadastrar"),),
+                    Divider(),
+                    TextButton(
+                        onPressed: (){
+                          setState(() {
+                            getIn = !getIn;
+                          });
+                        },
+                        child: Text((getIn) ? "Ainda não tem uma conta? Cadastre-se!" : "Já tem uma consta? Entre!"),
+                    ),
+                  ],
+                ),
+              ),
+            ),
           ),
         ],
       ),
